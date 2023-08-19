5ee83b6eb4aeb72a2213f9540448b17bcbde371e
Juan Nacl <jgarciaot@unal.edu.co>
2023-08-18 23:00:16 -0500

    Adición y modificacion escalas foto V1.01

--- lib/UI/view/home_view.dart
+++ lib/UI/view/home_view.dart
@@ -1,8 +1,9 @@
-import 'package:animate_do/animate_do.dart';
+
 import 'package:flutter/material.dart';
 import 'package:glitcheffect/glitcheffect.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:portafolio/UI/view/theme/colors.dart';
+import 'package:animate_do/animate_do.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
@@ -11,17 +12,14 @@ class HomeView extends StatelessWidget {
   Widget build(BuildContext context) {
     return const SingleChildScrollView(
       child: Center(
-          child: Column(
-        children: [
-          SizedBox(
-            height: 20,
-          ),
-          _DescriptionRow(),
-          SizedBox(
-            height: 50,
-          ),
-        ],
-      )),
+        child: Column(
+          children: [
+            SizedBox(height: 20),
+            _DescriptionRow(),
+            SizedBox(height: 50),
+          ],
+        ),
+      ),
     );
   }
 }
@@ -31,19 +29,48 @@ class _DescriptionRow extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final Size size = MediaQuery.of(context).size;
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        // Adjust the layout based on screen width
+        if (constraints.maxWidth >= 955) {
+          return _WideScreenLayout();
+        } else {
+          return _NarrowScreenLayout();
+        }
+      },
+    );
+  }
+}
+
+class _WideScreenLayout extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.start,
       children: [
-        Padding(
-          padding: EdgeInsets.only(left: size.width * 0.05),
-          child: const _InitialText(),
+        const Padding(
+          padding: EdgeInsets.only(left: 80),
+          child: _InitialText(),
         ),
         Expanded(child: Container()),
-        Padding(
-          padding: EdgeInsets.only(right: size.width * 0.05),
-          child: const _InitialPhoto(),
-        )
+        const Padding(
+          padding: EdgeInsets.only(right: 20),
+          child: _InitialPhoto(),
+        ),
+        const SizedBox(width: 80)
+      ],
+    );
+  }
+}
+
+class _NarrowScreenLayout extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+        _InitialText(),
+        SizedBox(height: 50),
+        _InitialPhoto(),
       ],
     );
   }
@@ -58,8 +85,8 @@ class _InitialPhoto extends StatelessWidget {
       duration: const Duration(milliseconds: 1000),
       child: Container(
         padding: const EdgeInsets.only(right: 25),
-        width: 400,
-        height: 400,
+        width: 380, // Adjust the width as needed
+        height: 380, // Adjust the height as needed
         decoration: BoxDecoration(
           color: backgroundColor,
           borderRadius: BorderRadius.circular(360),
@@ -80,6 +107,8 @@ class _InitialPhoto extends StatelessWidget {
 class _InitialText extends StatelessWidget {
   const _InitialText();
 
+  final double scaleFactor = 1.0;
+  
   @override
   Widget build(BuildContext context) {
     return FadeInLeftBig(
@@ -90,21 +119,21 @@ class _InitialText extends StatelessWidget {
           Text(
             'HI',
             style: GoogleFonts.roboto(
-                fontSize: 50, fontWeight: FontWeight.bold, color: textColor),
+                fontSize: 40, fontWeight: FontWeight.bold, color: textColor),
           ),
           GlitchEffect(
-            colors:const  [Colors.green, auxColor,primaryColor],
+            colors: const [Colors.green, auxColor, primaryColor],
             duration: const Duration(milliseconds: 2500),
             child: Text(
               'I\'m Juan Pablo Garcia',
               style: GoogleFonts.roboto(
-                  fontSize: 50, fontWeight: FontWeight.bold, color: textColor),
+                  fontSize: 40, fontWeight: FontWeight.bold, color: textColor),
             ),
           ),
           Text(
             'a mobile Developer',
             style: GoogleFonts.roboto(
-                fontSize: 50, fontWeight: FontWeight.bold, color: textColor),
+                fontSize: 40, fontWeight: FontWeight.bold, color: textColor),
           ),
         ],
       ),
